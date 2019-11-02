89afec15e8cce15daaad2d4a4ef707ad7a2a62b9
Logan Trevorrow <trevorrl@umich.edu>
2019-11-02 17:08:47 -0400

    Added a test for malfformed input

--- rpn.py
+++ rpn.py
@@ -16,6 +16,8 @@ def calculate(string):
 		else:
 			stack.append(int(token))
 		print(stack)
+	if len(stack) != 1:
+		raise TypeError('Malformed input: ' + string)
 	return stack.pop()
 
 def main():
--- test_rpn.py
+++ test_rpn.py
@@ -8,3 +8,6 @@ class TestBasics(unittest.TestCase):
 	def test_sub(self):
 		result = rpn.calculate("5 3 -")
 		self.assertEqual(2, result)
+	def test_badinput(self):
+		with self.assertRaises(TypeError):
+			rpn.calculate('1 2 3 +')
